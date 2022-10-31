45bfb1cfd4ad490fc4102189bd988179f7b5fb1d
Александр <truba191@yandex.ru>
2022-11-01 00:20:27 +0300

    initial commit

---
+++ src/main/java/ru/kortunov/wordstress/WordStressApplication.java
@@ -0,0 +1,13 @@
+package ru.kortunov.wordstress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordStressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordStressApplication.class, args);
+	}
+
+}
